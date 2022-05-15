497b49cedd47b03ce4c062929d940962d7ae9fb1
David <kingdice@Atomic>
2022-05-16 00:44:06 +0300

    here we create a new method to check whether we can get news from a specific branch and the other is to check whether we can clear all.

--- src/main/java/Dao/Sql2oDepartmentsDao.java
+++ src/main/java/Dao/Sql2oDepartmentsDao.java
@@ -1,6 +1,7 @@
 package Dao;
 
 import models.Departments;
+import models.News;
 import models.Users;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -90,5 +91,29 @@ public class Sql2oDepartmentsDao implements DepartmentsDao{
 
             return users;
         }
+    }
+    @Override
+    public List<News> getDepartmentNews(int id) {
+        try(Connection con=sql2o.open()) {
+            String sql="SELECT * FROM news WHERE id=:id ";
+            return con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public void clearAll() {
+        try (Connection con=sql2o.open()){
+            String sql="DELETE FROM departments";
+            String sqlUsersDepartments="DELETE FROM users_departments";
+            con.createQuery(sql).executeUpdate();
+            con.createQuery(sqlUsersDepartments).executeUpdate();
+
+        }catch (Sql2oException e){
+            System.out.println(e);
+        }
+
+
     }
 }
