672900ba53e2c85cce4f7b7764069e6b4ede7a92
David <kingdice@Atomic>
2022-05-15 23:27:32 +0300

    here we are adding a test to check whether we can add an id  department for our department dao.

--- src/test/java/Dao/Sql2oDepartmentsDaoTest.java
+++ src/test/java/Dao/Sql2oDepartmentsDaoTest.java
@@ -1,8 +1,11 @@
 package Dao;
 
+import models.Departments;
+import models.Users;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -40,6 +43,27 @@ public class Sql2oDepartmentsDaoTest {
     public static void shutDown() throws Exception{
         conn.close();
         System.out.println("connection closed");
+    }
+    @Test
+    public void idSetForAddedDepartment() {
+        Departments department=setUpNewDepartment();
+        int originalId=department.getId();
+        sql2oDepartmentsDao.add(department);
+        assertNotEquals(originalId,department.getId());
+    }
+
+    @Test
+    public void addUserToDepartment() {
+        Departments department=setUpNewDepartment();
+        sql2oDepartmentsDao.add(department);
+        Users user=setUpNewUser();
+        Users otherUser= new Users("Dennis","treasury","cash transfers");
+        sql2oUsersDao.add(user);
+        sql2oUsersDao.add(otherUser);
+        sql2oDepartmentsDao.addUserToDepartment(user,department);
+        sql2oDepartmentsDao.addUserToDepartment(otherUser,department);
+        assertEquals(2,sql2oDepartmentsDao.getAllUsersInDepartment(department.getId()).size());
+        assertEquals(2,sql2oDepartmentsDao.findById(department.getId()).getSize());
     }
 
 }
